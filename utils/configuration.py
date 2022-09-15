1f74a4841f67a27a31f16d6098af33d6d4f5e95d
Jędrzej <jedrzej.polaczek@gmail.com>
2022-09-15 23:19:12 +0200

    fix: correct get_json_data file path variable.

--- utils/configuration.py
+++ utils/configuration.py
@@ -2,14 +2,16 @@ from loguru import logger
 import json
 
 
-def get_json_data() -> dict:
+def get_json_data(file_path: str) -> dict:
     """ 
     Read JSON dict from file.
+
+    file_path (str): path to JSON file.
     
     return dict: dict based on read JSON file.
     """
     logger.info("Opening JSON file.")
-    json_file = open('config.json')
+    json_file = open(file_path)
     
     logger.info("Returns JSON object as a dictionary.")
     data = json.load(json_file)
