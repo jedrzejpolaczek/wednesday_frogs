39ea7ed25599b1d54a72d16cbdb2131c539e10bf
Jędrzej <jedrzej.polaczek@gmail.com>
2022-09-21 00:21:30 +0200

    feat: added initial args.

--- run.py
+++ run.py
@@ -4,15 +4,19 @@ from loguru import logger
 from model.run import run_model_training
 from email_frog.run import run_email_sender
 from discord.run import run_bot
+from utils import get_args
 
-# TODO: add args
-# TODO: make it configurable from args
+
+args = get_args()
+
 logger.remove()
-logger.add(sys.stderr, level="INFO")
+logger.add(sys.stderr, level=args.logger_level)
 
-# TODO: add logic here based on args
-# run_model_training()
-# run_email_sender()
-# run_bot()
-
-
+if args.type_of_run == "train":
+    run_model_training()
+elif args.type_of_run == "email":
+    run_email_sender()
+elif args.type_of_run == "discord":
+    run_bot()
+else:
+    logger.error("Invalid run type. You can only choose type_of_run bewteen train, email or discord!")
--- utils.py
+++ utils.py
@@ -4,7 +4,22 @@ import os
 import tensorflow
 from datetime import datetime
 from xmlrpc.client import Boolean
+import argparse
+
 from model.networks.generator import create_generator
+
+
+def get_args() -> dict:
+    """ Get args from command line using argparse library. """
+    parser = argparse.ArgumentParser(description='Process Wednesday frogs arguments.') 
+
+    parser.add_argument('--type_of_run', type=str, help='\You can choose type of run bewteen \
+        train for training the model, \
+            email for generate and email frog image or \
+                discord for run discord bot.')
+    parser.add_argument('--logger_level', type=str, default="INFO", help='Level of logs to be displayed.')
+
+    return parser.parse_args()
 
 
 def get_json_data(file_path: str) -> dict:
@@ -24,8 +39,9 @@ def get_json_data(file_path: str) -> dict:
     return data
 
 
+# TODO: add typing and return type
 def generate_images(model, seed, json_path: str="training_config.json"):
-    """ TODO: add docstring"""
+    """ TODO: add docstring """
     # Notice `training` is set to False.
     # This is so all layers run in inference mode (batchnorm).
     seed = get_seed(json_path)
@@ -35,6 +51,7 @@ def generate_images(model, seed, json_path: str="training_config.json"):
 
 
 def get_seed(json_path: str="training_config.json"):
+    """ TODO: add docstring """
     model_data = get_json_data(json_path)
 
     noise_dim = model_data["noise_dim"]
@@ -45,7 +62,8 @@ def get_seed(json_path: str="training_config.json"):
     return seed
 
 
-def save_image(save_dir, name, images):
+# TODO: add typing
+def save_image(save_dir: str, name: str, images):
     """ TODO: add docstring"""
     img = tensorflow.keras.utils.array_to_img(images[0] * 255., scale=False)
     img.save(os.path.join(save_dir, f'{name}.png'))
@@ -64,7 +82,8 @@ def is_it_wednesday() -> Boolean:
     return is_it_wednesday
 
 
-def load_model(json_path: str="training_config.json") -> tensorflow.Model:
+# TODO: add return type
+def load_model(json_path: str="training_config.json"):
     """ TODO: Add docstring """
     model_data = get_json_data(json_path)
 
